96060cec82a9e54fae07c6d1649708bb64916c45
yyoannle <yoann.letacq1@gmail.com>
2024-06-29 19:12:11 +0200

    small fix

--- nmap/scan_udp.py
+++ nmap/scan_udp.py
@@ -7,20 +7,21 @@ def scan_udp(host, port):
     try:
         # Create a UDP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(5)
+        sock.settimeout(1)
 
         # Send an empty packet to the target
         sock.sendto(b'', (host, port))
 
-        try:
-            # Wait for a response
-            _data, _addr = sock.recvfrom(1024)
+        # Wait for a response
+        res = sock.recvfrom(1024)
 
+        if res:
             sock.close()
             service = get_service(port)
             return f"Port {port} is open, Service: {service}"
-        except socket.timeout:
-            sock.close()
+        else:
             return f"Port {port} is closed"
+
     except socket.error as e:
+
         return f"Cannot connect to {host}:{port}, error: {e}"
